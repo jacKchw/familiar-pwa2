a2205b04bb079b56437bcaf8138c94ba6c594564
jacKchw <aesopw56220@gmail.com>
2020-12-20 18:22:50 +0800

    deploy

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -19,6 +19,7 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
+  
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
@@ -30,9 +31,12 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
+      console.log('isLocalhost');
+
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
+        console.log('isLocalhost');
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
 
